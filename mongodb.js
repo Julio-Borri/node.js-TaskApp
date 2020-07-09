e29702d8dbd0cede62e969e72a7bea03f9fcd1de
Julio-Borri <juliodborri@gmail.com>
2020-07-09 14:28:14 -0300

    Section 10 - done

--- mongodb.js
+++ mongodb.js
@@ -1,22 +1,20 @@
 // CRUD Create, Read, Update and Delete
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID} = require('mongodb');
 
-const contectionURL = 'mongodb://127.0.0.1:27017';
+const contectionURL = 'mongodb://127.0.0.1:27017'; // 'localhost' is causing some strange issues
 const databaseName = 'task-manager';
 
+// const id = new ObjectID();
+// console.log(id.id.length);
+// console.log(id.getTimestamp());
+// console.log(id.toHexString().length);
+
 MongoClient.connect(contectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-  if (error) {
-    return console.log('Unable to connect to database!');
-  }
+  if (error) return console.log('Unable to connect to database!');
 
   const db = client.db(databaseName);
  
-  db.collection('users').insertOne({
-    name: 'Lucas',
-    age: 36
-  })
-  .then(res => console.log(res.ops))
-  .catch(error => console.log('Unable to insert user!'))
-})
+  db.collection('users').deleteOne({ name: 'Ernesto' })
+    .then(res => console.log(res))
+});
